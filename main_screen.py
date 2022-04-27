e2e6ef01d702cb26ca5847591801bab39ef04ef7
smlozaydin <samil.bilal.ozaydin@gmail.com>
2022-04-27 08:31:02 +0300

    the beginning

---
+++ main_screen.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+
+resolution = (1024, 1024)
+screen = pygame.display.set_mode(resolution)
+pygame.display.set_caption("Street Fighters!")
+
+
+def play():
+
+    situation = True
+    while situation:
+
+
+def creditim():
+    situation2 = True
+    while situation2:
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event in pygame.QUIT:
+            run = False
+
+pygame.quit()
+exit()
